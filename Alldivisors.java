22b06f9812da27fb51826260a16b05600ab80d89
ekta2321 <123200112+ekta2321@users.noreply.github.com>
2026-03-10 00:37:37 +0530

    Create Alldivisors.javfind all divisors in O(sqrt(n)) using partners logic
    - Optimized divisor finding from O(N) to O(sqrt(N)) by utilizing the "Partner" property.
    - Handled perfect square edge cases to avoid duplicate entries.
    - Used ArrayList and Collections.sort() for ascending order output.

---
+++ Alldivisors.java
@@ -0,0 +1,24 @@
+class Solution {
+    public static void print_divisors(int n) {
+        // code here
+        ArrayList<Integer>list = new ArrayList<>();
+        for(int i = 1 ; i*i <=n ;  i++){
+            
+            if(n % i == 0 ){
+                list.add(i);
+                if(i != n/i ){
+                    list.add(n/i);
+                }
+                
+                
+            }
+            
+        }
+        Collections.sort(list);
+        for(int i : list){
+            System.out.print(i + " ");
+        }
+       
+    }
+    
+}

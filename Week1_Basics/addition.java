b793afe5a2f02414db2eb309f04581d19cbac4d5
ekta2321 <123200112+ekta2321@users.noreply.github.com>
2026-07-01 23:07:41 +0530

    addition of two numbers
    Logic: Basic arithmetic summation of two integers.
    Key Learning: Initial setup to verify repository compilation baselines.

---
+++ Week1_Basics/addition.java
@@ -0,0 +1,22 @@
+//https://www.geeksforgeeks.org/problems/arithmetic-operators-1605594922/1
+
+
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        // code here
+        int p = x + y ;
+        int q =  x -y;
+        int r = x * y;
+        int s = x/y;
+        int t = x % y;
+
+        // The below code prints the output
+        System.out.println(p + " " + q + " " + r + " " + s + " " + t);
+    }
+}
